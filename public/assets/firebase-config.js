0c6ac70e432fbdc6881734cbd80d5e6fae9c54bc
techmankapapa <thenordicsmp@gmail.com>
2026-09-06 00:04:26 +0530

    Add files via upload

--- public/assets/firebase-config.js
+++ public/assets/firebase-config.js
@@ -21,12 +21,12 @@
 --------------------------------------------------------- */
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID",
+  apiKey: "AIzaSyAlYaA49RM8dQ7hfI_9mcBq97Q33cExiAI",
+  authDomain: "lothkunta-lanja.firebaseapp.com",
+  projectId: "lothkunta-lanja",
+  storageBucket: "lothkunta-lanja.firebasestorage.app",
+  messagingSenderId: "924282063292",
+  appId: "1:924282063292:web:dc51b26d4460ed1ca903b9",
 };
 
 firebase.initializeApp(firebaseConfig);
